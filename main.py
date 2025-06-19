6e3219b4d30bf658882d9643f9004f6578de91ac
Maksimov24 <anpologov-2019@mail.ru>
2025-06-19 17:32:46 +0300

    Базовое окно

---
+++ main.py
@@ -0,0 +1,25 @@
+import pygame
+import sys
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption('Морской Бой')
+    
+    clock = pygame.time.Clock()
+    running = True
+    
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+        screen.fill((0, 0, 255))  # Синий фон
+        pygame.display.flip()
+        clock.tick(60)
+    
+    pygame.quit()
+    sys.exit()
+
+if __name__ == "__main__":
+    main()
