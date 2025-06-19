8bb4ad2ae55cba649c166fa81907371ad3145b6d
Maksimov24 <anpologov-2019@mail.ru>
2025-06-19 17:40:24 +0300

    Класс Game и состояния игры

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
 import pygame
 import sys
 from constants import *
+from game import Game
 
 def main():
     pygame.init()
+    pygame.font.init()
+    
     screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     pygame.display.set_caption('Морской Бой')
     
-    clock = pygame.time.Clock()
-    running = True
-    
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-        
-        screen.fill(BLUE)
-        pygame.display.flip()
-        clock.tick(60)
+    game = Game(screen)
+    game.run()
     
     pygame.quit()
     sys.exit()
